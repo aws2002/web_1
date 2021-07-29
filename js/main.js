14db342c59681ba29f0ac54bdcf28b2f28d32fa7
Osama Husam <80280073+aws2002@users.noreply.github.com>
2021-07-29 12:19:56 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+new WOW().init();
+
+$.scrollIt({
+    topOffset: -50
+});
+
+
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '30px',
+//     duration: 2000,
+//     reset: true
+// });
+
+// sr.reveal(``, {
+//     interval: 200
+// })
